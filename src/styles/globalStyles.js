dbd159933f8c0557d1dd91f88130348b2684de5f
HLHD-CREW <87148091+HLHD-CREW@users.noreply.github.com>
2022-02-16 10:17:24 -0500

    Update globalStyles.js

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -6,7 +6,7 @@ export const Screen = styled.div`
   background-image: ${({ image }) => (image ? `url(${image})` : "none")};
  background-size: contain;
 background-repeat: no-repeat;
-    max-width: 100%;
+    max-width: 60%;
 
   overflow: hidden ;
   
